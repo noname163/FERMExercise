823143bd9a66216ad6d893d3fca6a958d92e6f0b
DangHuuDat <danghuudat163@gmail.com>
2022-07-19 12:15:18 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
 
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     console.log(this.props); 
-//     this.state = {
-//       dishes: DISHES
-//     };
-//   }
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navbar dark color="primary">
-//           <div className="container">
-//             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-//           </div>
-//         </Navbar>
-//         <Menu dishes={this.state.dishes} />
-//       </div>
-//     );
-//   }
-// } 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(this.props); 
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+} 
 
 class App extends Component {
 
